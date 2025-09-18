7b2dc3c172a9997ef1ec5c9b93d9bc73e2228b97
Reza Memarian <r.memarian@hotmail.com>
2025-09-18 16:09:23 +0330

    This Practice is completed

--- 01-linux-admin/p157-e1.sh
+++ 01-linux-admin/p157-e1.sh
@@ -2,5 +2,15 @@
 #
 read -p "Enter the first number: " num1
 read -p "Enter the second number:" num2
-
-echo $num1
+if [ $num1 -eq $num2 ]
+then
+	echo "the first number is eqaul the second."
+elif [ $num1 -gt $num2 ]
+then
+	echo "the first number is grater than the second."
+elif [ $num1 -lt $num2 ]
+then
+	echo "the first number is less than the second."
+else
+	echo "The Entry is invalid."
+fi
